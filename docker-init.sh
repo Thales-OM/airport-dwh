c6395d91759578c12e1153d67539a0c19549c18c
il.pugin <il.pugin@tbank.ru>
2024-12-20 22:14:17 +0300

    append docker-init.sh

--- docker-init.sh
+++ docker-init.sh
@@ -34,4 +34,15 @@ sh ./debezium/connector-setup.sh
 
 docker-compose up -d kafdrop
 
+docker-compose up -d dwh_dds
+
+docker-compose up -d dmp_service
+
+docker-compose up -d db_analytical
+
+docker-compose up -d airflow
+
+docker exec -i airflow airflow connections add dwh_dds --conn-type 'postgres' --conn-login 'postgres' --conn-password 'postgres' --conn-host 'dwh_dds' --conn-port '5432' --conn-schema 'dwh_detailed'
+docker exec -i airflow airflow connections add db_analytical --conn-type 'postgres' --conn-login 'postgres' --conn-password 'postgres' --conn-host 'db_analytical' --conn-port '5432' --conn-schema 'presentation'
+
 echo "Done"
